12a549d3fe19c5d9e6127307e75fceab0549e024
Rockey Simmons <codenamerockey@gmail.com>
2019-10-11 10:59:46 -0700

    I completed the post end points for challenge

--- api/server.js
+++ api/server.js
@@ -50,12 +50,64 @@ server.get('/api/resources', (req, res) => {
       res.status(200).json(resource);
     })
     .catch(err => {
-      res
-        .status(500)
-        .json({
-          message:
-            'there was a problem retrieving the resources from the database'
+      res.status(500).json({
+        message:
+          'there was a problem retrieving the resources from the database'
+      });
+    });
+});
+
+server.post('/api/resources', (req, res) => {
+  db('resources')
+    .insert(req.body, 'id')
+    .then(ids => {
+      const id = ids[0];
+
+      db('resources')
+        .where({ id })
+        .first()
+        .then(resource => {
+          res.status(201).json(resource);
         });
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
+
+server.post('/api/projects', (req, res) => {
+  db('projects')
+    .insert(req.body, 'id')
+    .then(ids => {
+      const id = ids[0];
+
+      db('projects')
+        .where({ id })
+        .first()
+        .then(resource => {
+          res.status(201).json(projects);
+        });
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
+
+server.post('/api/tasks', (req, res) => {
+  db('tasks')
+    .insert(req.body, 'id')
+    .then(ids => {
+      const id = ids[0];
+
+      db('tasks')
+        .where({ id })
+        .first()
+        .then(task => {
+          res.status(201).json(task);
+        });
+    })
+    .catch(error => {
+      res.status(500).json(error);
     });
 });
 
